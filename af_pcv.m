cb45731a3a9148f52038edf23ebfbc5672b4b69e
GiangFredrik <fredrik.giang02@gmail.com>
2024-04-24 10:58:01 +0200

    Update af_pcv.m

---
+++ af_pcv.m
@@ -0,0 +1,8 @@
+%% P_cv, coefficient of variation x(n), dispersion
+
+
+
+w_size = 8 % window size in seconds, based on qrs
+w_interval = % rr-interval based on window size
+w_std = std()
+w_mean = mean()
